1492d0ad71068aa1896b619a17ac041af43f51ff
ezegyfa2 <darvasvok@gmail.com>
2023-07-14 20:08:51 +0300

    Refactoring

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Ezegyfa\LaravelHelperMethods\DynamicTemplateMethods;
 use App\Http\Controllers\Controller;
 
 class HomeController extends Controller
 {
     public function welcome() {
-        $template = (object) [
-            'type' => 'dynamic-template',
-            'data' => (object) [
-                'template_type_name' => 'ecom_welcome',
-                'params' => (object) [],
-            ]
-        ];
-        return view('layouts.dynamicPage', compact('template'));
+        return DynamicTemplateMethods::getTemplateDynamicPage('ecom_welcome', [], 'app');
+    }
+
+    public function products() {
+        return DynamicTemplateMethods::getTemplateDynamicPage('ecom_products', [], 'app');
     }
 }
